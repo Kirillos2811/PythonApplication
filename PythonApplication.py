c30c0bb981f0c4f8f1708a38c02e242ccf9ca749
Kirillos2811 <kirill.sesorov@gmail.com>
2023-05-23 21:31:33 +0300

    Lesson 7 task 1

--- PythonApplication.py
+++ PythonApplication.py
@@ -1,8 +1,5 @@
-n = int(input("n = "))
-
-count = 0
-for i in range(n):
-    if int(input()) == 0:
-        count += 1
-
-print("count =", count)
+string = input()
+if string == string[::-1]:
+    print("yes")
+else:
+    print("no")
