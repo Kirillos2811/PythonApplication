bdae4d437254a12d779a60a24ef88662b5ef88cd
Kirillos2811 <kirill.sesorov@gmail.com>
2023-06-09 21:11:23 +0300

    Lesson 13 task 1

--- PythonApplication.py
+++ PythonApplication.py
@@ -1,12 +1,29 @@
-def factorial(a):
-    res = 1
-    for i in range(1, a + 1):
-        res *= i
-    return res
+import random as rnd
 
-a = int(input())
-lst = []
-for i in reversed(range(1, factorial(a) + 1)):
-    lst.append(factorial(i))
+def print_list(lst):
+    for item  in lst:
+        print(*item)
 
-print(lst)
+n = int(input("Rows number = "))
+m = int(input("Columns number = "))
+
+matrix1 = [[0 for i in range(m)] for i in range(n)]
+matrix2 = [[0 for i in range(m)] for i in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        matrix1[i][j] = rnd.randint(1, 100)
+        matrix2[i][j] = rnd.randint(1, 100)
+
+res_matrix = [[0 for i in range(m)] for i in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        res_matrix[i][j] = matrix1[i][j] + matrix2[i][j]
+
+print("Matrix 1:")
+print_list(matrix1)
+print("Matrix 2:")
+print_list(matrix2)
+print("Resulting matrix:")
+print_list(res_matrix)
