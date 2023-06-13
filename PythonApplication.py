ca92eef9c18c63de6833de5467940a210bc08498
Kirillos2811 <kirill.sesorov@gmail.com>
2023-06-13 16:33:03 +0300

    Lesson 14 task 1

--- PythonApplication.py
+++ PythonApplication.py
@@ -1,29 +1,9 @@
-import random as rnd
+def print_list(index, lst):
+    print(lst[index], end=" ")
+    if index == len(lst) - 1:
+        print("Конец списка")
+    else:
+        print_list(index + 1, lst)
 
-def print_list(lst):
-    for item  in lst:
-        print(*item)
-
-n = int(input("Rows number = "))
-m = int(input("Columns number = "))
-
-matrix1 = [[0 for i in range(m)] for i in range(n)]
-matrix2 = [[0 for i in range(m)] for i in range(n)]
-
-for i in range(n):
-    for j in range(m):
-        matrix1[i][j] = rnd.randint(1, 100)
-        matrix2[i][j] = rnd.randint(1, 100)
-
-res_matrix = [[0 for i in range(m)] for i in range(n)]
-
-for i in range(n):
-    for j in range(m):
-        res_matrix[i][j] = matrix1[i][j] + matrix2[i][j]
-
-print("Matrix 1:")
-print_list(matrix1)
-print("Matrix 2:")
-print_list(matrix2)
-print("Resulting matrix:")
-print_list(res_matrix)
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+print_list(0, my_list)
