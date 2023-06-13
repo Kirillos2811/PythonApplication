0d5a5ea9268248b374bf0ecda469fffac6b0d3eb
Kirillos2811 <kirill.sesorov@gmail.com>
2023-06-14 02:08:18 +0300

    Lesson 16 task 1

--- PythonApplication.py
+++ PythonApplication.py
@@ -1,15 +1,29 @@
-class Transport:
-    def __init__(self, name, max_speed, mileage):
-        self.name = name
-        self.max_speed = max_speed
-        self.mileage = mileage
+class CashRegister:
+    def __init__(self, cash):
+        self.cash = cash
 
-class Autobus(Transport):
-    def __init__(self, name, max_speed, mileage):
-        super().__init__(name, max_speed, mileage)
+    def top_up(self, x):
+        self.cash += x
 
-    def __str__(self):
-        return f"Название автомобиля: {self.name} Скорость: {self.max_speed} Пробег: {self.mileage}"
+    def count_1000(self):
+        return self.cash // 1000
 
-autobus = Autobus("Mini Van", 150, 15)
-print(autobus)
+    def take_away(self, x):
+        if self.cash < x:
+            raise ValueError(f"Unable to take away {x}: not enough cash.")
+        self.cash -= x
+
+    def get_cash(self):
+        return self.cash
+
+cash_register = CashRegister(2000)
+cash_register.top_up(500)
+print(cash_register.get_cash())
+
+print(cash_register.count_1000())
+
+cash_register.take_away(2000)
+print(cash_register.get_cash())
+
+cash_register.take_away(700)
+print(cash_register.get_cash())
